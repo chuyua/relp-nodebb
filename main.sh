bf83179b55f9f1c2c8409e2e055a42da3803f268
chuyua <979093525@qq.com>
2022-04-30 17:18:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,7 @@
 # rm -rf artalk-go* data/ #Uncomment this line to update
 if [ ! -f "nodebb" ];then
 git clone https://github.com/NodeBB/NodeBB
-
 chmod +x ./NodeBB
-
 mv -b NodeBB/* ./ && mv -b NodeBB/.[^.]* ./ && rm -rf *~ && rm -rf NodeBB
 fi
 redis-server ./redis.conf
