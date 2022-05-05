9a5a3e28f86ec62649330070bb9ac6c7c8b6489c
chuyua <979093525@qq.com>
2022-05-05 14:48:36 +0800

    Delete main.sh

--- main.sh
+++
@@ -1,9 +0,0 @@
-# rm -rf artalk-go* data/ #Uncomment this line to update
-if [ ! -f "nodebb" ];then
-git clone https://github.com/NodeBB/NodeBB
-chmod +x ./NodeBB
-mv -b NodeBB/* ./ && mv -b NodeBB/.[^.]* ./ && rm -rf *~ && rm -rf NodeBB
-fi
-redis-server ./redis.conf
-./nodebb stop
-./nodebb start
